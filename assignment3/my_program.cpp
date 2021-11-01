9ced6cc761bbcec3b9f50d409ade16fd7fe6255e
Ubuntu <ubuntu@ip-172-31-22-33.us-east-2.compute.internal>
2021-11-01 02:23:40 +0000

    comment

--- assignment3/my_program.cpp
+++ assignment3/my_program.cpp
@@ -7,20 +7,13 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  ifstream fin("mymoves.dat",ios::in | ios::out);
-  ofstream fout("mymoves.dat",ios::app);
   stack<string> moves;
       if (strcmp(argv[2], "zero") == 0) {
                 cout << "silent";
         } else {
-                fout << argv[2] << "\n";
-                string s;
-                while (getline(fin, s)) {
-                   moves.push(s);
-                 }
+		moves.push(argv[2]);
                 cout << moves.top();
         }
-  fin.close();
-  fout.close();
   return 0;
 }
+
